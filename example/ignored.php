a378088a35a1976614aeecb137a40fb13f706a9e
Felippe <felippemoraes@bitbucket.org>
2018-12-20 17:25:08 -0200

    some changes to better show the example

--- example/ignored.php
+++ example/ignored.php
@@ -1,4 +1,2 @@
 <!--  This file are setted to be ignored, so changes here will not trigger an autoreload -->
-<div class="ignored">
-  <span>:)</span>
-</div>
+<p class="small">Php Hot Reloader by Felippe Regazio</p>
